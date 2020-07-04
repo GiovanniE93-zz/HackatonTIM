8a21fdb6d8061087c2589da39a02cbab239c3166
Giovanni Erpete <gianni.erpete93@gmail.com>
2020-07-04 16:43:48 +0200

    ok tab bar

---
+++ TIMHackaton/TIMHackaton/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TIMHackaton
+//
+//  Created by Giovanni Erpete on 04/07/2020.
+//  Copyright © 2020 Giovanni Erpete. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
